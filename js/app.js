f38f306eae1178e6e8c482ef3d2584556734e901
Kresimir I. Nurnberger <kin9006@aol.com>
2016-02-25 20:07:10 +0100

    search is working, need filter title

--- js/app.js
+++ js/app.js
@@ -1,17 +1,53 @@
- function searchKeyword() {
-            gapi.client.setApiKey('AIzaSyDmEnC0k6Qw7F0s7KV5DuFFwjQ0CPT96sE');
-            gapi.client.load('youtube', 'v3', function() {
-                    makeRequest();
-            });
-    }
-    function makeRequest() {
-            var q = $('#query').val();
-            var request = gapi.client.youtube.search.list({
-                       q: q,
-                    part: 'snippet'                        
-            });
-            request.execute(function(response) {
-                    var str = JSON.stringify(response.result);
-                    $('#search-results').html('<p>' + str + '</p>');
-            });
-    }
+// search is working but the results are not scrabbled
+function searchKeyword() {
+        gapi.client.setApiKey('AIzaSyCWNxek5Rd0cdkpWoqxrKMYLu4mGsS6GZY');
+        gapi.client.load('youtube', 'v3', function() {
+        	makeRequest();
+        });
+}
+function makeRequest() {
+        var searchTerm = $('#query').val();
+        var request = gapi.client.youtube.search.list ({
+                    q: searchTerm,
+                    part: 'snippet',
+                    maxResults: 1,
+                    type: 'video' });
+
+        request.execute(function(item) {
+                var str = JSON.stringify(item.items);
+                $('#search-results').append('<p>' + str + '</p>');
+        });
+}
+// function showTitle(object, [key, value]) {
+
+// }
+
+ // $(function(){
+// 	  $('#search-button').click(function(event){
+// 	    event.preventDefault();
+// 	    var searchTerm = $('#query').val();
+// 	    getRequest(searchTerm);
+// 	  });
+// });
+
+// function getRequest(searchTerm){
+// 	  var params = {
+// 	        q: searchTerm,
+// 	        part: 'snippet',
+// 	        maxResults: 2  
+// 	  };
+// 	  url = 'https://www.googleapis.com/youtube/v3/search';
+
+// 	  $.getJSON(url, params, function(data){
+// 	    showResults(data.Search);
+// 	  });
+// }
+
+// function showResults(results){
+// 	  var html = "";
+// 	  $.each(results, function(index,value){
+// 	    html += '<p>' + value.Title + '</p>';
+// 	    console.log(value.Title);
+// 	  });
+// 	  $('#search-results').html(html);
+// } 
