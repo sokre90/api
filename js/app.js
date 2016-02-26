a4c80799db75e4bab2f69654027caaf326b3d9de
Kresimir I. Nurnberger <kin9006@aol.com>
2016-02-26 17:24:02 +0100

    title search working, but can fetch only one specific title

--- js/app.js
+++ js/app.js
@@ -1,53 +1,65 @@
-// search is working but the results are not scrabbled
+// title search is working but can only fetch one title not all of them
 function searchKeyword() {
         gapi.client.setApiKey('AIzaSyCWNxek5Rd0cdkpWoqxrKMYLu4mGsS6GZY');
         gapi.client.load('youtube', 'v3', function() {
-        	makeRequest();
+        	
+       		makeRequest();
+        		
         });
+
 }
+
 function makeRequest() {
-        var searchTerm = $('#query').val();
+  
+		var searchTerm = $('#query').val();   
         var request = gapi.client.youtube.search.list ({
                     q: searchTerm,
                     part: 'snippet',
-                    maxResults: 1,
+                    maxResults: 5,
                     type: 'video' });
 
-        request.execute(function(item) {
-                var str = JSON.stringify(item.items);
-                $('#search-results').append('<p>' + str + '</p>');
-        });
-}
-// function showTitle(object, [key, value]) {
+	    request.execute (function(item) {	
+		    var str = JSON.stringify(item.items[0].snippet.title);
+	    	$('#search-results').append('<p>' + str + '</p>');
+  	
+	    });
+} 
 
-// }
+	
 
- // $(function(){
-// 	  $('#search-button').click(function(event){
-// 	    event.preventDefault();
-// 	    var searchTerm = $('#query').val();
-// 	    getRequest(searchTerm);
-// 	  });
-// });
 
-// function getRequest(searchTerm){
-// 	  var params = {
-// 	        q: searchTerm,
-// 	        part: 'snippet',
-// 	        maxResults: 2  
-// 	  };
-// 	  url = 'https://www.googleapis.com/youtube/v3/search';
+        
 
-// 	  $.getJSON(url, params, function(data){
-// 	    showResults(data.Search);
-// 	  });
-// }
 
-// function showResults(results){
-// 	  var html = "";
-// 	  $.each(results, function(index,value){
-// 	    html += '<p>' + value.Title + '</p>';
-// 	    console.log(value.Title);
-// 	  });
-// 	  $('#search-results').html(html);
-// } 
+// var searchTerm = $('#query').val();
+// $(function(){
+
+// 	function searchKeyword() {
+// 	        gapi.client.setApiKey('AIzaSyCWNxek5Rd0cdkpWoqxrKMYLu4mGsS6GZY');
+// 	        gapi.client.load('youtube', 'v3', function() {
+// 	        	getRequest();
+// 	        });
+// 	}
+
+// 	function getRequest(searchTerm){
+// 		  var params = ({
+// 		        q: searchTerm,
+// 		        part: 'snippet',
+// 		        maxResults: 2  
+// 		  });
+// 		  // url = 'https://www.googleapis.com/youtube/v3/search';
+
+// 		  $.get(url, params, function(data){
+// 		    showResults(data.Search);
+// 		  });
+// 	}
+
+// 	function showResults(results){
+// 		  var html = "";
+// 		  $.each(results, function(index,value){
+// 		    html += '<p>' + value.Title + '</p>';
+// 		    console.log(value.Title);
+// 		  });
+// 		  $('#search-results').html(html);
+// 	} 
+// })
